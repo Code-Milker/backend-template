46859228f82935b832c2ab3e740a674a03d25622
Milk-Maven <tylerf66@gmail.com>
2024-05-24 17:15:50 -0500

    update template repo

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,62 @@
-/**
- * Some predefined delay values (in milliseconds).
- */
-export enum Delays {
-  Short = 500,
-  Medium = 2000,
-  Long = 5000,
+
+import express, { Request, Response, NextFunction } from 'express';
+import bodyParser from 'body-parser';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+interface User {
+  username: string;
+  password: string;
 }
 
-/**
- * Returns a Promise<string> that resolves after a given time.
- *
- * @param {string} name - A name.
- * @param {number=} [delay=Delays.Medium] - A number of milliseconds to delay resolution of the Promise.
- * @returns {Promise<string>}
- */
-function delayedHello(
-  name: string,
-  delay: number = Delays.Medium,
-): Promise<string> {
-  return new Promise((resolve: (value?: string) => void) =>
-    setTimeout(() => resolve(`Hello, ${name}`), delay),
-  );
-}
+const users: User[] = []; // This will act as our "database" for this example
 
-// Please see the comment in the .eslintrc.json file about the suppressed rule!
-// Below is an example of how to use ESLint errors suppression. You can read more
-// at https://eslint.org/docs/latest/user-guide/configuring/rules#disabling-rules
+// Secret key for JWT
+const JWT_SECRET = 'your_jwt_secret_key';
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export async function greeter(name: any) { // eslint-disable-line @typescript-eslint/no-explicit-any
-  // The name parameter should be of type string. Any is used only to trigger the rule.
-  return await delayedHello(name, Delays.Long);
-}
+// Middleware to protect routes
+const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (!token) return res.sendStatus(401);
+
+  jwt.verify(token, JWT_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+};
+
+app.post('/register', async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  users.push({ username, password: hashedPassword });
+
+  res.status(201).send('User registered');
+});
+
+app.post('/login', async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+
+  const user = users.find(user => user.username === username);
+  if (!user) return res.status(400).send('Cannot find user');
+
+  const validPassword = await bcrypt.compare(password, user.password);
+  if (!validPassword) return res.status(400).send('Incorrect password');
+
+  const accessToken = jwt.sign({ username: user.username }, JWT_SECRET, { expiresIn: '1h' });
+  res.json({ accessToken });
+});
+
+app.get('/protected', authenticateToken, (req: Request, res: Response) => {
+  res.send('This is a protected route');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
